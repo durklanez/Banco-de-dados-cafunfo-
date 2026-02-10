0cc48310ec17833cedb17dbda8030c6b3ea820fb
durklanez <durkjosue@gmail.com>
2026-02-10 21:14:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
-import banco
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
-CORS(app)
 
+# Rota principal (abre o site)
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/usuarios", methods=["GET"])
-def listar():
-    return jsonify(banco.listar_usuarios())
+# API teste (status do banco)
+@app.route("/api/status")
+def status():
+    return jsonify({
+        "status": "ok",
+        "mensagem": "Banco de dados Cafunfo ativo"
+    })
+
+# Exemplo de API para cadastrar dados
+@app.route("/api/cadastrar", methods=["POST"])
+def cadastrar():
+    dados = request.json
+
+    nome = dados.get("nome")
+    produto = dados.get("produto")
+
+    return jsonify({
+        "status": "sucesso",
+        "nome": nome,
+        "produto": produto
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
